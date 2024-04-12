48013d678cb5a3a0e08cafd2ec9a754e07f45d0a
Petrik Diak <diak@petrik.hu>
2024-04-12 10:19:48 +0200

    Feltöltése ezekenk az alapoknak

---
+++ src/Oldalak/Layout.tsx
@@ -0,0 +1,26 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export function Layout() {
+  return (
+    <>
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/"></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Outlet />
+    </>
+  )
+}
