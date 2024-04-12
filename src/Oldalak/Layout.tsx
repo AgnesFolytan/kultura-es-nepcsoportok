6bc0c9141a24065262ecc929cd7476b73c6439aa
Folytán Ágnes <folytanagnes@gmail.com>
2024-04-12 10:27:40 +0200

    test

--- src/Oldalak/Layout.tsx
+++ src/Oldalak/Layout.tsx
@@ -12,7 +12,7 @@ export function Layout() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/">Home valami</Nav.Link>
               <Nav.Link href="/"></Nav.Link>
             </Nav>
           </Navbar.Collapse>
