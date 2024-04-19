0ed72240b9ea705e45f7c1e7f29425fbbdf6b75b
Domigy <domi.adamerik@gmail.com>
2024-04-19 09:43:00 +0200

    FireBase frissités

--- src/confige.tsx
+++ src/confige.tsx
@@ -15,6 +15,9 @@ const firebaseConfig = {
   appId: "1:574801949497:web:8eef38f0105f30daeb6ee8",
   measurementId: "G-24HFWM2LQD"
 };
+const firebase= require('firebase');
+firebase.initializeApp(firebaseConfig);
+const db= firebase.firestore();
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
