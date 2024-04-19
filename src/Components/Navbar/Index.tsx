ab9cee200bd51d4ce8835030056316337ad017b4
AgnesFolytan <folytanagnes@gmail.com>
2024-04-19 09:58:43 +0200

    kinézet

--- src/Components/Navbar/Index.tsx
+++ src/Components/Navbar/Index.tsx
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './Index.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { Container } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,13 +7,14 @@ import Navbar from 'react-bootstrap/Navbar';
 export function NavBar(){
     return <div id="app">
     <Navbar expand="lg" className="bg-body-tertiary">
+        <span className="navbar-brand" id='text'>Kúltúra és Népcsoportok</span>
         <Container>
           <Navbar.Brand href="#home"></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">Home valami</Nav.Link>
-              <Nav.Link href="/"></Nav.Link>
+              <Nav.Link href="/">Még valami</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
