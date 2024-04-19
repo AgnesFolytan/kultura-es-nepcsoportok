c4f3671b4722a78a60f832934584eeeab4603ada
AgnesFolytan <folytanagnes@gmail.com>
2024-04-19 09:41:44 +0200

    Layout alapok

--- src/Oldalak/Layout.tsx
+++ src/Oldalak/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { NavBar } from "../Components/NavBar/Index";
 
 export function Layout() {
   return (
     <>
-      
+      <NavBar />
 
       <Outlet />
     </>
